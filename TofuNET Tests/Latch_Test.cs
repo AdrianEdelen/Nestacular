75953612e43a13528fa458d849376b12f2e17ac2
Adrian Edelen <adrianedelen@gmail.com>
2022-08-12 16:14:03 -0400

    Added srlatch and tests

---
+++ TofuNET Tests/Latch_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TofuNET_Tests
+{
+    [TestClass]
+    internal class Latch_Test
+    {
+
+        [TestMethod]
+        public void SRLatchTest()
+        {
+            var latch = new SRLatch();
+            Assert.IsTrue( new Func<bool>(() =>
+            {
+                bool q;
+                bool notq;
+                latch.R = false;
+                latch.S = false;
+                q = latch.Q;
+                notq = latch.NotQ;
+                return true; /* dummy, still building the test */
+            })());
+
+
+        }
+
+    }
+}
---
+++ TofuNET/Latches/SRLatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TofuNET.Gates;
+
+namespace TofuNET.Latches
+{
+    public class SRLatch
+    {
+        
+        
+        public bool Q => _rGate.Output;
+        public bool NotQ => _sGate.Output;
+
+
+        public bool R
+        {
+            set
+            {
+                _rGate.Input1 = value;
+                _rGate.Input2 = NotQ;
+            }
+        }
+        public bool S
+        {
+            set
+            {
+                _sGate.Input1 = Q;
+                _sGate.Input2 = value;
+            }
+        }
+
+        private NOR _rGate = new NOR();
+        private NOR _sGate = new NOR();
+
+
+
+    }
+}
